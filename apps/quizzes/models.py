a665373d856e12cd3578239bde87e06f14d9d9cc
BarathKumar-G <barathkumargmoorthy@gmail.com>
2026-03-20 22:33:32 +0530

    Add core models: Quiz, Question, Attempt, Answer

--- apps/quizzes/models.py
+++ apps/quizzes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Quiz(models.Model):
+    topic = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=50)
+    total_questions = models.IntegerField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.topic
+    
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    text = models.TextField()
+    options = models.JSONField()
+    correct_answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
