bcef3ea8dfa1ef6a6a94b247e38a79c716905354
BarathKumar-G <barathkumargmoorthy@gmail.com>
2026-03-20 22:21:12 +0530

    Implement JWT authentication using SimpleJWT

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path("api/users/", include("apps.users.urls")),
+    #JWT
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
